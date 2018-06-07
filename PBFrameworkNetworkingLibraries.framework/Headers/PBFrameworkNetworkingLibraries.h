fb665d7f9fc4640ab6f82176dd960aa95596cf01
Daniel Spencer <nerdromancer@gmail.com>
2018-06-07 01:06:57 -0700

    Added umbrella header

---
+++ PBFrameworkNetworkingLibraries.framework/Headers/PBFrameworkNetworkingLibraries.h
@@ -0,0 +1,14 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for PBFrameworkNetworkingLibraries.
+FOUNDATION_EXPORT double PBFrameworkNetworkingLibrariesVersionNumber;
+
+//! Project version string for PBFrameworkNetworkingLibraries.
+FOUNDATION_EXPORT const unsigned char PBFrameworkNetworkingLibrariesVersionString[];
+
+#include <PBFrameworkNetworkingLibraries/ExifContainer.h>
+#include <PBFrameworkNetworkingLibraries/Base64.h>
+#include <PBFrameworkNetworkingLibraries/NRFileManager.h>
+#include <PBFrameworkNetworkingLibraries/UIImage+Exif.h>
+#include <PBFrameworkNetworkingLibraries/PBNetworkingInfo.h>
+#include <PBFrameworkNetworkingLibraries/Reachability.h>
